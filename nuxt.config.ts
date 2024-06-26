cf8d4eb1ee0951089f424afbfe9925d9204a2254
Iris-Fla <irith.fla@gmail.com>
2024-06-27 01:47:06 +0900

    Faviconの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   css: ["~/assets/css/default.css", "bootstrap/scss/bootstrap.scss"],
   app: {
     head: {
-      title: "SuirenMeru",
+      title: "メルの図書館",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
