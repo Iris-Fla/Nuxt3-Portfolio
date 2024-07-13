2f904d1d99e3094e58cb84ebf7da70669fe646de
Iris-Fla <irith.fla@gmail.com>
2024-07-13 23:27:06 +0900

    tocを追加し、記事のUXを向上

--- app/pages/[slug]/index.vue
+++ app/pages/[slug]/index.vue
@@ -1,10 +1,9 @@
 <script setup>
-import 'highlight.js/styles/agate.css';
+import "highlight.js/styles/agate.css";
 import * as cheerio from "cheerio";
 import hljs from "highlight.js";
 
-
-import { gsap } from 'gsap';
+import { gsap } from "gsap";
 
 const route = useRoute();
 const slug = route.params.slug;
@@ -24,14 +23,13 @@ useHead({
   ],
 });
 
-
 const $ = cheerio.load(article.value.content);
 $("pre code").each((_, elm) => {
   const result = hljs.highlightAuto($(elm).text());
   $(elm).html(result.value);
   $(elm).addClass("hljs");
 });
-$('img').each((_, element) => {
+$("img").each((_, element) => {
   $(element).addClass(`custom-image`);
 });
 $("table").each((_, element) => {
@@ -41,27 +39,44 @@ $("table").each((_, element) => {
 const body = $.html();
 
 onMounted(() => {
-  gsap.to('.thumbnail-image', { borderRadius: '30px', duration: 1 });
+  gsap.to(".thumbnail-image", { borderRadius: "30px", duration: 1 });
 });
 </script>
 
 <template>
   <Container>
     <main class="kiwi main-color">
-      <BH1 class="title">{{ article.title }}<span class="Cocoa"></span></BH1>
-      <b-p class="publishedAt">
-        投稿日:{{ $formatDate(String(article.publishedAt)) }}
-      </b-p>
-      <FormatImage :src="article.thumbnail.url" :alt="article.title" class="thumbnail-image" />
-      <Toc selector="#article-content" class="toc">
-        <Localization>
-          <template #en>
-            項目
-          </template>
-        </Localization>
-      </Toc>
-      <div v-if="body" class="article" v-html="body" id="article-content">
-      </div>
+      <Row justify-content="end">
+        <Col col="12 lg-8">
+        <BH1 class="title">{{ article.title }}<span class="Cocoa"></span></BH1>
+        <b-p class="publishedAt">
+          投稿日:{{ $formatDate(String(article.publishedAt)) }}
+        </b-p>
+        <FormatImage
+          :src="article.thumbnail.url"
+          :alt="article.title"
+          class="thumbnail-image"
+        />
+        <Toc selector="#article-content" class="toc d-lg-none">
+          <Localization>
+            <template #en> 項目 </template>
+          </Localization>
+        </Toc>
+        <div
+          v-if="body"
+          class="article"
+          v-html="body"
+          id="article-content"
+        ></div>
+        </Col>
+        <Col col="lg-2">
+          <Toc selector="#article-content" class="toc d-none d-lg-block sticky-toc">
+            <Localization>
+              <template #en> 項目 </template>
+            </Localization>
+          </Toc>
+        </Col>
+      </Row>
     </main>
   </Container>
 </template>
@@ -85,12 +100,54 @@ onMounted(() => {
   border-style: none none none solid;
 }
 
-.toc :hover {
+.toc li :hover {
   border-color: #8bdaff;
   color: #8bdaff !important;
 }
-.toc :hover #text{
-  background-color: #8bdaff;
+
+.toc a {
+  position: relative; /* 疑似要素の配置の基準点とする */
+}
+
+.toc a::after {
+  content: ""; /* 疑似要素にはcontentが必要ですが、ここでは空にします */
+  position: absolute;
+  width: 100%; /* 親要素と同じ幅 */
+  height: 100%; /* 親要素と同じ高さ */
+  left: 0;
+  top: 0;
+  background-color: rgba(206, 206, 206, 0.2) ;
+  z-index: -1; /* 要素の後ろに配置 */
+  opacity: 0; /* 初期状態では透明 */
+  transition: opacity 0.3s; /* アニメーション効果 */
+}
+
+.toc a:hover::after {
+  opacity: 1; /* ホバー時に背景色を表示 */
+}
+
+.sticky-toc {
+  position: sticky;
+  top: 100px;
+}
+
+.sticky-toc hr {
+  overflow: visible;
+  border-top: none;
+  height: 3px;
+  position: relative;
+  opacity: 1;
+}
+
+.sticky-toc hr::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 3px;
+  background-image: linear-gradient(to right, #8bdaff 50%, transparent 50%);
+  background-size: 10px 3px;
 }
 
 .custom-image {
@@ -100,7 +157,7 @@ onMounted(() => {
   width: 100%;
   height: auto;
   box-shadow: 0 0 10px #bdbdbd;
-  margin-top:20px;
+  margin-top: 20px;
   margin-bottom: 20px;
 }
 
@@ -145,7 +202,7 @@ h1::after {
 }
 
 h2 {
-  color:rgb(0, 0, 0);
+  color: rgb(0, 0, 0);
   margin-top: 40px;
   margin-bottom: 20px;
   position: relative;
@@ -179,5 +236,4 @@ pre {
   margin-top: 2px;
   margin-bottom: 2px;
 }
-
 </style>
