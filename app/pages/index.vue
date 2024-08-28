e4c307a3a075ff0141c65561433840491f0e36fe
Meru🔮 <irith.fla@gmail.com>
2024-08-28 23:53:18 +0900

    fix: Fetch時のローディング状態の追加

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-const { data: blog } = await useFetch('/api/blog');
+const { data: blog, status } = await useFetch('/api/blog');
 
 useHead({
   title: '記事一覧',
@@ -9,24 +9,27 @@ useHead({
     { property: 'og:type', content: 'website' }
   ]
 })
-
 </script>
+
 <template>
   <Container padding="10px">
-    <Row>
+    <div v-if="status === 'pending' || status ==='idle'" class="loading-indicator">
+      <p>読み込み中...!</p>
+    </div>
+    <Row v-else>
       <Col col="12 xl-3 lg-4 md-6" v-for="article in blog.contents" :key="article.id">
-      <NuxtLink :to="`/${article.id}`" style="text-decoration: none; color: inherit;">
-        <Card margin="b-3" :to="`/${article.id}`" class="card-style">
-          <NuxtImg :src="`${article.thumbnail.url}?fm=webp`" :alt="article.title" fit="cover" format="webp" placeholder quality="80"/>
-          <CardBody style="padding-top: 7px;">
-            <CardText><SkillIcons :useSkill= "$formatTags(article.tags)" /></CardText>
-            <CardTitle margin="b-3">{{ article.title }}</CardTitle>
-            <CardText small style="opacity: 0.8;">
-              <BIcon margin="e-1" icon="bi:clock" />{{ $formatDate(String(article.publishedAt)) }}
-            </CardText>
-          </CardBody>
-        </Card>
-      </NuxtLink>
+        <NuxtLink :to="`/${article.id}`" style="text-decoration: none; color: inherit;">
+          <Card margin="b-3" :to="`/${article.id}`" class="card-style">
+            <NuxtImg :src="`${article.thumbnail.url}?fm=webp`" :alt="article.title" fit="cover" format="webp" placeholder quality="80"/>
+            <CardBody style="padding-top: 7px;">
+              <CardText><SkillIcons :useSkill= "$formatTags(article.tags)" /></CardText>
+              <CardTitle margin="b-3">{{ article.title }}</CardTitle>
+              <CardText small style="opacity: 0.8;">
+                <BIcon margin="e-1" icon="bi:clock" />{{ $formatDate(String(article.publishedAt)) }}
+              </CardText>
+            </CardBody>
+          </Card>
+        </NuxtLink>
       </Col>
     </Row>
   </Container>
@@ -53,5 +56,13 @@ useHead({
   border: 1px solid #c3def0;
   padding: 3px 8px;
   border-radius: 8px;
+}
+
+.loading-indicator {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  font-size: 1.2em;
 }
 </style>
