a2fadfd8690942a981715c73dab1fe85794994c0
Iris-Fla <irith.fla@gmail.com>
2024-09-03 18:02:12 +0900

    fix:情報の追加

--- app/pages/sample.vue
+++ app/pages/sample.vue
@@ -18,8 +18,58 @@
         <span>Iris-Fla</span>
       </h1>
       <b-p margin="t-4"
-        class="kosugi-maru-regular profile-text">制作が好きです。Web開発だけでなく,動画編集やデザインも...!<br />現在はNuxt.jsを学習中です...!</b-p>
+        class="kosugi-maru-regular profile-text">誰かが楽しんでもらえる作品の制作が好きです。Web開発だけでなく、動画編集やデザインも...!<br />現在はNuxt.jsを学習中です...!
+      </b-p>
     </div>
+    <h3 class="kosugi-maru-regular profile-text decorated-heading">
+      <span>使用言語</span>
+    </h3>
+    <h3 class="kosugi-maru-regular profile-text decorated-heading">
+      <span>コンテスト等</span>
+    </h3>
+    <Row margin="t-4" style="max-width: 800px;margin: 0 auto;">
+      <Col col="6">
+      <Card class="works-card">
+        <CardBody>
+          <Row>
+            <Col col="10">
+            <CardTitle class="title">インテル株式会社</CardTitle>
+            <CardText class="kosugi-maru-regular">
+              OpenVino学生コンテスト:最優秀賞✨<br />
+            </CardText>
+            </Col>
+            <Col col="2">
+            <b-a href="https://forest.watch.impress.co.jp/docs/special/1598339.html" margin="t-2" button="primary" class="button-style" target="_blank">
+              <BIcon icon="bi:bookmark-fill" />
+            </b-a>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col col="6">
+        <Card class="works-card">
+        <CardBody>
+          <Row>
+            <Col col="10">
+            <CardTitle class="title">日本マイクロソフト株式会社</CardTitle>
+            <CardText class="kosugi-maru-regular">
+              OpenAIを用いたアプリケーション開発<br />
+            </CardText>
+            </Col>
+            <Col col="2">
+            <b-a href="https://www.microsoft.com/ja-jp/industry/blog/microsoft-in-business/2024/04/05/case-study-utilization-of-azure-openai-service-by-students/" margin="t-2" button="primary" class="button-style" target="_blank">
+              <BIcon icon="bi:bookmark-fill" />
+            </b-a>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+      </Col>
+    </Row>
+    <h3 class="kosugi-maru-regular profile-text decorated-heading">
+      <span>デザイン(A4サイズ)</span>
+    </h3>
     <Row margin="t-4" class="row-cards">
       <Col col="4">
       <Card class="image-container">
@@ -28,7 +78,6 @@
           <p class="overlay-text kosugi-maru-regular">文化祭ポスター</p>
         </div>
       </Card>
-
       </Col>
       <Col col="4">
       <Card class="image-container">
@@ -56,7 +105,7 @@
         <CardBody>
           <CardTitle class="title">ロート製薬株式会社</CardTitle>
           <CardText class="kosugi-maru-regular">
-            skio vc ホワイトピールセラム<br/>Instagramショート動画の製作
+            skio vc ホワイトピールセラム<br />Instagramショート動画の製作
           </CardText>
         </CardBody>
       </Card>
@@ -66,7 +115,7 @@
         <CardBody>
           <CardTitle class="title">株式会社 Be,想空間</CardTitle>
           <CardText class="kosugi-maru-regular">
-            Youtube用のDIY動画の製作<br/>サムネイルの作成
+            Youtube用のDIY動画の製作<br />サムネイルの作成
           </CardText>
         </CardBody>
       </Card>
@@ -158,6 +207,28 @@ hr {
   }
 }
 
+.button-style {
+  background-color: $color-secondary;
+  border-color: $color-secondary;
+  border-radius: 50%;
+  box-shadow: 0 0 5px $color-secondary;
+
+  &:hover {
+    background-color: #4d5a3d;
+    border-color: #4d5a3d;
+  }
+
+  &:active {
+    background-color: #4d5a3d;
+    border-color: #4d5a3d;
+  }
+
+  &:focus {
+    background-color: $color-secondary;
+    border-color: $color-secondary;
+  }
+}
+
 .image-container:hover .overlay-circle {
   opacity: 1;
 }
@@ -229,6 +300,7 @@ hr {
 .works-card {
   background-color: $color-main;
   box-shadow: 0 0 8px $color-main;
+
   .title {
     color: $color-secondary;
   }
@@ -236,5 +308,6 @@ hr {
 
 .background {
   background-color: $color-secondary;
+  padding-bottom: 150px;
 }
 </style>
