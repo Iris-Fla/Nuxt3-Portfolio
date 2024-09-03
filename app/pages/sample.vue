4f29abb9523d710c1310bb552c99650577be1c14
Iris-Fla <irith.fla@gmail.com>
2024-09-03 12:38:02 +0900

    fix:プロフィールページの修正

--- app/pages/sample.vue
+++ app/pages/sample.vue
@@ -4,21 +4,74 @@
   <NuxtImg src="./Header.png" alt="Header" format="webp" class="header-image" />
   <Container type="fluid" class="background">
     <p class="typed-text kosugi-maru-regular">
-      <Typed
-        :strings="['にゃんはろ～！', 'プロフィールです。']"
-        :type-speed="50"
-        :back-delay="1000000"
-      />
+      <Typed :strings="['にゃんはろ～！', 'プロフィール', 'いろいろ書いてます']" :type-speed="50" :back-delay="8000" />
     </p>
     <hr />
     <div class="text-center">
-      <NuxtImg
-        src="./omen_bangboo.png"
-        alt="icon"
-        format="webp"
-        class="profile-icon"
-      />
+      <div class="image-container">
+        <NuxtImg src="./omen_bangboo.png" alt="icon" format="webp" class="profile-icon" />
+        <div class="overlay-circle">
+          <p class="overlay-text kosugi-maru-regular">Adobeillustratorで作成<br />"omen_bangboo"</p>
+        </div>
+      </div>
+      <h1 class="kosugi-maru-regular profile-text decorated-heading">
+        <span>Iris-Fla</span>
+      </h1>
+      <b-p margin="t-4"
+        class="kosugi-maru-regular profile-text">制作が好きです。Web開発だけでなく,動画編集やデザインも...!<br />現在はNuxt.jsを学習中です...!</b-p>
     </div>
+    <Row margin="t-4" class="row-cards">
+      <Col col="4">
+      <Card class="image-container">
+        <CardImg src="./pos_1.webp" alt="..." class="col-card" />
+        <div class="overlay">
+          <p class="overlay-text kosugi-maru-regular">文化祭ポスター</p>
+        </div>
+      </Card>
+
+      </Col>
+      <Col col="4">
+      <Card class="image-container">
+        <CardImg src="./pos_2.webp" alt="..." class="col-card" />
+        <div class="overlay">
+          <p class="overlay-text kosugi-maru-regular">サークルポスター</p>
+        </div>
+      </Card>
+      </Col>
+      <Col col="4">
+      <Card class="image-container">
+        <CardImg src="./pos_3.webp" alt="..." class="col-card" />
+        <div class="overlay">
+          <p class="overlay-text kosugi-maru-regular">サークルポスター2</p>
+        </div>
+      </Card>
+      </Col>
+    </Row>
+    <h3 class="kosugi-maru-regular profile-text decorated-heading">
+      <span>動画編集</span>
+    </h3>
+    <Row margin="t-4" style="max-width: 800px;margin: 0 auto;">
+      <Col col="6">
+      <Card class="works-card">
+        <CardBody>
+          <CardTitle class="title">ロート製薬株式会社</CardTitle>
+          <CardText class="kosugi-maru-regular">
+            skio vc ホワイトピールセラム<br/>Instagramショート動画の製作
+          </CardText>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col col="6">
+      <Card class="works-card">
+        <CardBody>
+          <CardTitle class="title">株式会社 Be,想空間</CardTitle>
+          <CardText class="kosugi-maru-regular">
+            Youtube用のDIY動画の製作<br/>サムネイルの作成
+          </CardText>
+        </CardBody>
+      </Card>
+      </Col>
+    </Row>
   </Container>
 </template>
 <style scoped lang="scss">
@@ -49,6 +102,136 @@ hr {
   width: 200px;
   height: auto;
   border-radius: 50%;
+  box-shadow: 0 0 8px $color-main;
+}
+
+.profile-text {
+  color: $color-main;
+}
+
+.image-container {
+  position: relative;
+  display: inline-block;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+
+  .overlay-text {
+    margin-top: 30px;
+    color: $color-main;
+    font-size: 1em;
+    text-align: center;
+  }
+}
+
+.overlay-circle {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+
+  .overlay-text {
+    margin-top: 30px;
+    color: $color-main;
+    font-size: 1em;
+    text-align: center;
+  }
+}
+
+.image-container:hover .overlay-circle {
+  opacity: 1;
+}
+
+.image-container:hover .overlay {
+  opacity: 1;
+}
+
+.decorated-heading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  span {
+    margin-top: 30px;
+    position: relative;
+    padding: 0 20px;
+
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      width: 60px;
+      height: 5px;
+      border-radius: 5px;
+      background-color: $color-main;
+    }
+
+    &::before {
+      left: -70px;
+    }
+
+    &::after {
+      right: -70px;
+    }
+
+    &::before::after,
+    &::after::after {
+      content: '';
+      position: absolute;
+      top: -2px;
+      width: 20px;
+      height: 5px;
+      border-radius: 50%;
+      background-color: $color-main;
+    }
+
+    &::before::after {
+      right: 0;
+    }
+
+    &::after::after {
+      left: 0;
+    }
+  }
+}
+
+.row-cards {
+  max-width: 1000px;
+  margin: 0 auto;
+
+  .col-card {
+    box-shadow: 0 0 8px $color-main;
+  }
+}
+
+.works-card {
+  background-color: $color-main;
+  box-shadow: 0 0 8px $color-main;
+  .title {
+    color: $color-secondary;
+  }
 }
 
 .background {
