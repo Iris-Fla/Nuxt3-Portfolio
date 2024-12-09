47ef245e87a8ff7f7bc9b97887110c13e63973ef
Iris-Fla <irith.fla@gmail.com>
2024-12-09 16:09:07 +0900

    不要なコードを削除し、レイアウトのサンプルを作成

--- app/components/header.vue
+++ app/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <Navbar class="kawaiiNav" expand="sm">
     <Container type="fluid" class="shippori-antique-b1-regular">
-      <NavbarBrand href="/" style="color:#33272a;">メルの図書館<BIcon margin="s-1" icon="bi:book"/></NavbarBrand>
+      <NavbarBrand href="/" style="color:#33272a;">メルの図書館</NavbarBrand>
       <NavbarToggler />
       <NavbarCollapse>
         <NavbarNavList>
